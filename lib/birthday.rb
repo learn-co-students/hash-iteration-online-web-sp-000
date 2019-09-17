8ab6e4c7ed4c87542788919ff82544ac7bb01e74
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-17 23:35:36 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,22 @@
 #   "Amanda" => 27
 # }
 
+
+# Our job is to write a method that operates on a hash of birthday kids and wishes them a happy birthday.
+
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |i, j|
+    puts "Happy Birthday #{i}! You are now #{j} years old!"
+  end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |i, j|
+    if j <= 12
+      puts "Happy Birthday #{i}! You are now #{j} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
+  
