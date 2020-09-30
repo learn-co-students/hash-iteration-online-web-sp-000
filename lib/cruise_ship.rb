ff61784b9909742e023dab1d895626913ad06c34
fut0k0 <cccabell@icloud.com>
2020-09-30 15:39:56 -0700

    Passing lab

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,3 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
 def select_winner(passengers)
-  # add the code snippet here!
+	passengers.map {|suite, name| suite == :suite_a && name[0] == "A" ? name : nil} .compact[0]
 end
