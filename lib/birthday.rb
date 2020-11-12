b12b83ac84b806d1f51790791a6b9a16a3f72d65
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 22:57:07 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,23 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def select_winner (birthday_kids)
+  winner = ""
+  birthday_kids.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+
+  winner
 end
 
-
+def happy_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end
+end
